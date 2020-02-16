6bc88bdd3f2bed789ca724b148380814c79fe2de
oteppei <t0092@icloud.com>
2020-02-16 15:50:28 +0900

    edit new.html.erb and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(
-      user_name = params[:name]
+      user_name = params[:name],
       password = params[:password]
     )
   end
   if @user.save
-    session:user_id = @user.id
+    session[:user_id] = @user.id
     flash[:notice] = "ユーザー登録が完了しました"
     redirect_to("/users/#{@user.id}")
   else
