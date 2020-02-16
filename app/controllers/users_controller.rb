9efc520fa112345e269a1298c79496112fb6ce61
oteppei <t0092@icloud.com>
2020-02-16 15:38:58 +0900

    edit User controller and create new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(
+      user_name = params[:name]
+      password = params[:password]
+    )
+  end
+  if @user.save
+    session:user_id = @user.id
+    flash[:notice] = "ユーザー登録が完了しました"
+    redirect_to("/users/#{@user.id}")
+  else
+    render(users/new)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # Users controller
+  get '/new' => "users#new"
+  post "/users/create" => "users#create"
+
+  # Home controller
   get '/' => "home#top"
   get 'about' => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
