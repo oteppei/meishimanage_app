f215cd54d65f9135722a183f43fb2c34d9f04b6f
oteppei <t0092@icloud.com>
2020-02-23 13:23:26 +0900

    edit user controller and new, show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
       user_name = params[:name],
       password = params[:password]
     )
-  end
-  if @user.save
-    session[:user_id] = @user.id
-    flash[:notice] = "ユーザー登録が完了しました"
-    redirect_to("/users/#{@user.id}")
-  else
-    render(users/new)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render(users/new)
+    end
   end
 
   def edit
