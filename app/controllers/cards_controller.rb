e6395f1e9d92ae12ddc3c663c18f35a9fb17762e
oteppei <t0092@icloud.com>
2020-02-28 13:33:21 +0900

    error for user#show by syntax error cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,16 +5,16 @@ class CardsController < ApplicationController
 
   def create
     @card = new.Card(
-      client_name = params[:name]
-      client_company = params[:company]
-      user_id = @current_user.user_id
-      relation = params[:relation]
+      client_name: params[:name]
+      client_company: params[:company]
+      user_id: @current_user.id
+      relation: params[:relation]
     )
     if @card.save
       flash[:notice] = "名刺登録が完了しました"
       redirect_to("/cards/#{@user.id}/index")
     else
-      render(cards/new)
+      render("cards/new")
     end
   end
 
